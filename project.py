3bc9c94cde86dd94f5b5eae8c998aaa7b6737552
Chahin <chahingam292@gmail.com>
2025-03-13 22:05:40 +0100

    reslover le conflit

--- project.py
+++ project.py
@@ -6,6 +6,16 @@ def somme(T):
     return S
 
 Data = [1, 3, 5]
+
 Som = sum(Data)
 print ('la somme est:',som)
+
+if Date:
+ print ('la somme est:',sum(Data))
+ print ('le min est:',min(Data))
+ print ('le max est:',max(Data))
+else:
+ print ('dossier vide')
+
+
 
